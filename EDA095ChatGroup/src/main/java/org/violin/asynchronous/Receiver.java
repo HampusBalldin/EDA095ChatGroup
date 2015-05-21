28294c3f9756ed110e97de6cb42a08d6c61fa334
HampusBalldin <hampus.balldin@gmail.com>
2015-05-21 18:11:55 +0200

    Hej!

--- EDA095ChatGroup/src/main/java/org/violin/asynchronous/Receiver.java
+++ EDA095ChatGroup/src/main/java/org/violin/asynchronous/Receiver.java
@@ -35,30 +35,30 @@ public class Receiver implements Runnable {
 			System.out.println("After Get Message: " + msg);
 			sendToClient(os, msg);
 		}
-	}	
-			
+	}
+
 	public void terminate() {
-		running = false;								//	System.out.println("Inside receiver terminate");
-		exchanges.notifyAll();							//	System.out.println("Notify Exchanges");
+		running = false; // System.out.println("Inside receiver terminate");
+		exchanges.notifyAll(); // System.out.println("Notify Exchanges");
 		messageQueue.notifyAll();
 	}
-	
+
 	public void addToReplyQueue(Message msg) {
 		synchronized (messageQueue) {
 			messageQueue.add(msg);
 			messageQueue.notify();
 		}
 	}
-	
+
 	public void addReplyExchange(HttpExchange exchange) {
 		synchronized (exchanges) {
 			exchanges.add(exchange);
 			exchanges.notify();
 		}
 	}
-	
+
 	private Message retrieveFromReplyQueue() {
-		synchronized (messageQueue) {			
+		synchronized (messageQueue) {
 			while (messageQueue.size() == 0 && running) {
 				try {
 					messageQueue.wait();
@@ -70,10 +70,10 @@ public class Receiver implements Runnable {
 			return messageQueue.poll();
 		}
 	}
-	
+
 	private HttpExchange getReplyExchange() {
 		synchronized (exchanges) {
-			while (exchanges.size() == 0 && running) {		
+			while (exchanges.size() == 0 && running) {
 				try {
 					exchanges.wait();
 				} catch (InterruptedException e) {
@@ -88,11 +88,15 @@ public class Receiver implements Runnable {
 	private void sendToClient(OutputStream os, Message msg) {
 		if (running) {
 			QName qName = new QName("Message");
-			JAXBElement<Message> jaxbElement = new JAXBElement<Message>(qName, Message.class, msg);
+			JAXBElement<Message> jaxbElement = new JAXBElement<Message>(qName,
+					Message.class, msg);
 			try {
 				XMLUtilities.marshal(jaxbElement, ObjectFactory.class, os);
+				os.flush();
 			} catch (JAXBException e) {
-				e.printStackTrace();	
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
