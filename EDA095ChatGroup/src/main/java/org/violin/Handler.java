a93cdc11c3feb995debcb59f6625e40b7b2b0c8f
Joel <Joel@10.3.37.127>
2015-05-21 13:03:42 +0200

    DynamicHandler mm

--- EDA095ChatGroup/src/main/java/org/violin/Handler.java
+++ EDA095ChatGroup/src/main/java/org/violin/Handler.java
@@ -1,10 +1,77 @@
 package org.violin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import org.json.XML;
+import org.violin.database.DBUsers;
+import org.violin.database.Database;
+import org.violin.database.XMLUtilities;
+import org.violin.database.generated.Message;
+import org.violin.database.generated.ObjectFactory;
+import org.violin.database.generated.User;
+
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public abstract class Handler implements HttpHandler {
-	public boolean authenticate(HttpExchange exchange) {
-		return true;
+	protected Database db = new Database(); //ok att ha denn trots att db finns i subklasser?
+	protected DBUsers dbUsers = new DBUsers(db);
+	
+	protected boolean authenticate(User user) {
+		if (dbUsers.authenticate(user)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+	
+	protected User createUser(HttpExchange exchange) {
+		Headers headers = exchange.getResponseHeaders();
+		ArrayList<String> cookies = new ArrayList<String>();
+		cookies = (ArrayList<String>) headers.get("Cookie");
+		String[] cookie = cookies.get(0).split(";"); 	//"uid=" + user.getUid() + ";" + "pwd=" + user.getPwd());
+		String uid = cookie[0];
+		String pwd = cookie[1];
+		User user = new User();
+		user.setPwd(uid);
+		user.setPwd(pwd);
+		return user;
+	}
+			
+	protected String getExchangeContent(HttpExchange exchange) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				exchange.getRequestBody()));
+		StringBuilder sb = new StringBuilder();
+		try {
+			while (in.ready()) {
+				sb.append(in.readLine());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+
+	protected Message createMessage(String jsonString) {
+		JSONObject jsonObject = new JSONObject(jsonString);
+		String xml = XML.toString(jsonObject);
+		System.out.println(jsonString);
+		System.out.println(xml);
+		Message msg = null;
+		try {
+			msg = XMLUtilities.unmarshal(XMLUtilities.documentify(xml),
+					Message.class, ObjectFactory.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return msg;
+	}
+	
+	
+
 }
